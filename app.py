37dcf4d5ba51492368e2467d8aaa47e5657e3b0f
Mohamed Ben Achour <mbenachour24@gmail.com>
2024-12-31 07:15:21 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,7 @@
+# This file is part of the OPTMUS project.
+# Licensed under CC BY-NC 4.0. Non-commercial use only.
+# For more details, see the LICENSE file in the repository.
+
 from flask import Flask, jsonify, request, render_template
 from models.minioptimus import Society
 
