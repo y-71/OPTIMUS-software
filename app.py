4f3b99d0ee8ed9ba2c15037aa814bfa5a4bf9a4e
mbenachour24 <mbenachour24@gmail.com>
2025-01-04 13:53:36 +0100

    Fix: Bind to Heroku

--- app.py
+++ app.py
@@ -363,5 +363,6 @@ def init_app():
 
 socketio = SocketIO(app)
 
-if __name__ == '__main__':
-    socketio.run(app, debug=True, allow_unsafe_werkzeug=True)
+if __name__ == "__main__":
+    port = int(os.getenv("PORT", 5000))  # Default to 5000 if $PORT is not set
+    app.run(host="0.0.0.0", port=port)
